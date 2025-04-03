c09e209b6c525c98213f560f6f18c4846f139fa8
kaviyamagudeeswaran <kaviyamagudeeswaran3062005@gmail.com>
2025-04-03 23:58:33 +0530

    Delete level-10/Task-2/Task-1/weather directory

--- level-10/Task-2/Task-1/weather/weather/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css"; // ✅ Correct Bootstrap Import
-import "./styles/styles.css"; // ✅ Custom Styles
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
